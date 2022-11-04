ea198e3d514ecc9814637f24f04b233e5cf4bf3a
joimar755 <joimarjose19@gmail.com>
2022-11-04 00:48:48 -0500

    first commit

---
+++ loopbacks/Backend_Pedidos/src/models/pedido.model.ts
@@ -0,0 +1,59 @@
+import {Entity, model, property, belongsTo, hasOne} from '@loopback/repository';
+import {Persona} from './persona.model';
+import {Producto} from './producto.model';
+
+@model()
+export class Pedido extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  Id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Id_Producto: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Total: number;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  Estado: boolean;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Id_persona: string;
+
+  @belongsTo(() => Persona)
+  personaId: string;
+
+  @hasOne(() => Producto)
+  producto: Producto;
+
+  constructor(data?: Partial<Pedido>) {
+    super(data);
+  }
+}
+
+export interface PedidoRelations {
+  // describe navigational properties here
+}
+
+export type PedidoWithRelations = Pedido & PedidoRelations;
